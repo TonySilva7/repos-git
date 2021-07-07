8daa41c7091c5c1b1bc2df1040ce35b3041cfd3c
TonySilva7 <tony.deve7oper@gmail.com>
2021-07-07 12:32:59 -0300

    feat: create page inssues

--- src/Pages/Repository/index.js
+++ src/Pages/Repository/index.js
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
+import api from '../../services/api';
+import { BackButton, Container, IssuesList, Loading, Owner } from './styles';
 
 function Repository({ match }) {
+	const [repo, setRepo] = useState({});
+	const [issues, setIssues] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		async function load() {
+			const repoName = decodeURIComponent(match.params.repository);
+			const [repoData, issueData] = await Promise.all([
+				api.get(`/repos/${repoName}`),
+				api.get(`/repos/${repoName}/issues`, {
+					params: {
+						state: 'open',
+						per_page: 5,
+					},
+				}),
+			]);
+
+			setRepo(repoData.data);
+			setIssues(issueData.data);
+			setLoading(false);
+		}
+
+		load();
+	}, [match.params.repository]);
+
+	if (loading) {
+		return (
+			<Loading>
+				<h1>Carregando...</h1>
+			</Loading>
+		);
+	}
+
 	return (
-		<div>
-			<h1 style={{ color: '#FFF' }}>{decodeURIComponent(match.params.repository)}</h1>
-		</div>
+		<Container>
+			<BackButton to='/'>
+				<FaArrowLeft color='#0d2636' size={30} />
+			</BackButton>
+
+			<Owner>
+				<img src={repo.owner.avatar_url} alt={repo.owner.login} />
+				<h1>{repo.name}</h1>
+				<p>{repo.description}</p>
+			</Owner>
+
+			<IssuesList>
+				{issues.map((issue) => (
+					<li key={String(issue.id)}>
+						<img src={issue.user.avatar_url} alt={issue.user.login} />
+						<div>
+							<strong>
+								<a href={issue.html_url}>{issue.title}</a>
+								{issue.labels.map((label) => (
+									<span key={String(label.id)}>{label.name}</span>
+								))}
+							</strong>
+							<p>{issue.user.login}</p>
+						</div>
+					</li>
+				))}
+			</IssuesList>
+		</Container>
 	);
 }
 
---
+++ src/Pages/Repository/styles.js
@@ -0,0 +1,109 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: #fff;
+	height: 100vh;
+`;
+
+export const Container = styled.div`
+	max-width: 700px;
+	background: #fff;
+	border-radius: 4px;
+	box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+	padding: 30px;
+	margin: 80px auto;
+`;
+
+export const Owner = styled.header`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	img {
+		width: 100px;
+		border-radius: 50%;
+		margin: 20px 0;
+	}
+
+	h1 {
+		font-size: 30px;
+		color: #0d2636;
+	}
+
+	p {
+		margin-top: 5px;
+		font-size: 14px;
+		color: #000;
+		text-align: center;
+		line-height: 1.4;
+		max-width: 400px;
+	}
+`;
+
+export const BackButton = styled(Link)`
+	border: 0;
+	outline: 0;
+	background-color: transparent;
+`;
+
+export const IssuesList = styled.ul`
+	margin-top: 30px;
+	padding-top: 30px;
+	border-top: solid 1px #eee;
+	list-style: none;
+
+	li {
+		display: flex;
+		padding: 15px 10px;
+
+		& + li {
+			margin-top: 12px;
+		}
+
+		img {
+			width: 36px;
+			height: 36px;
+			border-radius: 50%;
+			box-shadow: 1px 3px 4px -2px rgba(18, 46, 102, 0.39);
+		}
+
+		div {
+			flex: 1;
+			margin-left: 12px;
+
+			p {
+				margin-top: 10px;
+				font-size: 12px;
+				color: #0d2636;
+			}
+
+			strong {
+				font-size: 15px;
+
+				a {
+					text-decoration: none;
+					color: #222;
+					transition: all 0.3s ease;
+
+					&:hover {
+						color: #0071db;
+					}
+				}
+
+				span {
+					background-color: #0d2636;
+					color: #fff;
+					border-radius: 4px;
+					font-size: 12px;
+					font-weight: 600px;
+					padding: 4px 7px;
+					margin-left: 10px;
+				}
+			}
+		}
+	}
+`;
